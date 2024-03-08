66eb96ced792886d0fc095c0c1ac599f0d59294d
rency1192 <88083974+rency1192@users.noreply.github.com>
2024-03-08 11:46:22 +0530

    Add files via upload

---
+++ polymorphism/by_functions.py
@@ -0,0 +1,18 @@
+# A simple Python function to demonstrate 
+# Polymorphism
+
+def add(x, y, z = 0): 
+	return x + y+z
+
+print(add(2, 3))
+print(add(2, 3, 4))
+
+# Python program to demonstrate in-built poly-
+# morphic functions
+print("---------------------")
+# len() being used for a string
+print(len("geeks"))
+
+# len() being used for a list
+print(len([10, 20, 30]))
+
